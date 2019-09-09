4dcb03d3cfc5704be2ae5e9802df79b865b19e6b
Gabriele <info@elk-lab.com>
2019-09-09 16:30:57 +0200

    prepare for serverside running

--- sct.js
+++ sct.js
@@ -3549,63 +3549,65 @@ process.umask = function() { return 0; };
 (function (Buffer){
 "use strict";
 
-var pcsc = require('@ap-mitch/pcsclite');
+var pcsclite = require('@ap-mitch/pcsclite');
 
-var pcsc = pcsc();
+var pcsc = pcsclite();
 pcsc.on('reader', function (reader) {
   console.log('New reader detected', reader.name);
   reader.on('error', function (err) {
-    console.log('Error(', this.name, '):', err.message);
+    console.log('Error(', reader.name, '):', err.message);
   });
   reader.on('status', function (status) {
-    console.log('Status(', this.name, '):', status);
-    /* check what has changed */
+    console.log('Status(', reader.name, '): ' + status.state); // check what has changed
 
-    var changes = this.state ^ status.state;
+    var changes = reader.state ^ status.state;
 
-    if (changes) {
-      if (changes & this.SCARD_STATE_EMPTY && status.state & this.SCARD_STATE_EMPTY) {
-        console.log("card removed");
-        /* card removed */
+    if (!changes) {
+      return;
+    }
 
-        reader.disconnect(reader.SCARD_LEAVE_CARD, function (err) {
+    if (status.state == 18) {
+      console.log("card removed");
+      reader.disconnect(reader.SCARD_LEAVE_CARD, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log('Disconnected');
+      });
+      return;
+    }
+
+    if (status.state == 34) {
+      console.log("card inserted");
+      reader.connect({
+        share_mode: reader.SCARD_SHARE_SHARED
+      }, function (err, protocol) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log('Protocol(', reader.name, '): ', protocol);
+        reader.transmit(Buffer.from([0x00, 0xB0, 0x00, 0x00, 0x20]), 40, protocol, function (err, data) {
           if (err) {
             console.log(err);
-          } else {
-            console.log('Disconnected');
+            return;
           }
-        });
-      } else if (changes & this.SCARD_STATE_PRESENT && status.state & this.SCARD_STATE_PRESENT) {
-        console.log("card inserted");
-        /* card inserted */
 
-        reader.connect({
-          share_mode: this.SCARD_SHARE_SHARED
-        }, function (err, protocol) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log('Protocol(', reader.name, '):', protocol);
-            reader.transmit(new Buffer([0x00, 0xB0, 0x00, 0x00, 0x20]), 40, protocol, function (err, data) {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log('Data received', data);
-                reader.close();
-                pcsc.close();
-              }
-            });
-          }
+          console.log('Data received', data);
         });
-      }
+      });
+      return;
     }
   });
   reader.on('end', function () {
-    console.log('Reader', this.name, 'removed');
+    console.log('Reader ', reader.name, ' removed');
   });
 });
 pcsc.on('error', function (err) {
-  console.log('PCSC error', err.message);
+  console.log('PCSC error ', err.message);
 });
 
 }).call(this,require("buffer").Buffer)
